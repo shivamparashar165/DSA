e97332f37e5a503aad931787b4293f90bc1e9bc2
Shivam Jha <69099519+shivamparashar165@users.noreply.github.com>
2021-07-28 11:26:42 +0530

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,16 +1,17 @@
-void buubleSort(int arr[], int n){
-	for(int i=0; i<n; i++){
-		bool swapped = false;
-		for(int j=0; j<n-i-1; j++){
-			if(arr[i]>arr[j]){
-				int temp = arr[i];
-				arr[i]=arr[j];
-				arr[j]=temp;
-				swapped = true;
-			}
-		}
-		if(swapped == false){
-			break;
-		}
-	}
+void bubbleSort(int arr[], int n){
+    
+    bool swapped;
+    for(int i = 0;i < n; i++){
+        
+        swapped = false;
+        for(int j = 0 ; j < n - i -1; j++){
+            if( arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+        }
+        
+        if( swapped == false)
+        break;
+    }
 }
