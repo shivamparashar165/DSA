350ec5e08a1a34c3d18e0608f7f3d30342b96b2a
Shivam Jha <69099519+shivamparashar165@users.noreply.github.com>
2021-07-28 10:33:07 +0530

    Create bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,11 +1,16 @@
 void buubleSort(int arr[], int n){
 	for(int i=0; i<n; i++){
+		bool swapped = false;
 		for(int j=0; j<n-i-1; j++){
 			if(arr[i]>arr[j]){
 				int temp = arr[i];
 				arr[i]=arr[j];
 				arr[j]=temp;
+				swapped = true;
 			}
+		}
+		if(swapped == false){
+			break;
 		}
 	}
 }
